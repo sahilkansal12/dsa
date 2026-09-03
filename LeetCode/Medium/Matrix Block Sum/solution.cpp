30322862f222c5d52fc005adcd5fee45e913e0a6
sahilkansal12 <sahilkansal12@users.noreply.github.com>
2026-09-03 06:30:30 +0530

    Solved: Matrix Block Sum [Medium]

--- LeetCode/Medium/Matrix Block Sum/solution.cpp
+++ LeetCode/Medium/Matrix Block Sum/solution.cpp
@@ -15,31 +15,41 @@ public:
 //         return total;
 //     }
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+      vector<vector<int>> ans;  
+       int n=mat.size();
+        int m=mat[0].size();
         
-        int m=mat.size();
-        int n=mat[0].size();
-        vector<vector<int>> prefix(m+1,vector<int>(n+1,0));
-        for(int i=1;i<=m;i++)
+        ans=mat;
+       for(int i=0;i<n;i++)
         {
-            for(int j=1;j<=n;j++)
-            {                
-                prefix[i][j]=mat[i-1][j-1]+prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1];
-                
-            }
-
+        for(int j=1;j<m;j++)
+         {
+            ans[i][j]=ans[i][j-1]+mat[i][j];
+          }
         }
+       for(int i=1;i<n;i++)
+       {
+         for(int j=0;j<m;j++)
+         {
+            ans[i][j]=ans[i-1][j]+ans[i][j];
+         }
+       }
         int r1, c1, r2,c2;
-        vector<vector<int>> result(m+1,vector<int>(n+1,0));
-        for(int i=0;i<m;i++)
+     
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
                 r1=max(i-k,0);
                 c1=max(j-k,0);
-                r2=min(i+k,m-1);
-                c2=min(j+k,n-1);
-                r1++, r2++,c1++, c2++;
-               mat[i][j]=prefix[r2][c2]-prefix[r1 - 1][c2]-prefix[r2][c1 - 1]+prefix[r1 - 1][c1 - 1];
+                r2=min(i+k,n-1);
+                c2=min(j+k,m-1);
+                int sum=ans[r2][c2];
+             
+             if(r1>0) sum=sum-ans[r1 - 1][c2];
+             if(c1>0) sum=sum-ans[r2][c1 - 1];
+             if(r1>0&&c1>0) sum=sum+ans[r1 - 1][c1 - 1];
+            mat[i][j]=sum;
             }
 
         }

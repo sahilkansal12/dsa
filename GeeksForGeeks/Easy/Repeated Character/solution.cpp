8d05acfcc97479766a9b5e6afc4a1f9b766b8a77
sahilkansal12 <sahilkansal12@users.noreply.github.com>
2026-09-12 11:59:28 +0530

    Solved: Repeated Character [Easy]

--- GeeksForGeeks/Easy/Repeated Character/solution.cpp
+++ GeeksForGeeks/Easy/Repeated Character/solution.cpp
@@ -1,18 +1,31 @@
 class Solution {
   public:
     char firstRep(string s) {
-     unordered_map<char,int>mp;
+//      unordered_map<char,int>mp;
      int n=s.size();
+//      for(int i=0;i<n;i++)
+//      {
+//          mp[s[i]]++;
+//      }
+     
+//      for(int i=0;i<n;i++)
+//      {
+//          if(mp[s[i]]>1) return s[i];
+//      }
+            
+//   return '#';
+ 
+    int fre[26]={0};
      for(int i=0;i<n;i++)
      {
-         mp[s[i]]++;
+         fre[s[i]-'a']++;
      }
      
      for(int i=0;i<n;i++)
      {
-         if(mp[s[i]]>1) return s[i];
+         if(fre[s[i] -'a']>1) return s[i];
      }
-            
-   return '#';
+     
+     return '#';
     }
 };
